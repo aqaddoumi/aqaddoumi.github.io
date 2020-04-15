4f49a2ffcada715ed4362c411d352f14aab61375
aqaddoumi <qaddoumi.adel@gmail.com>
2020-04-15 17:14:15 +0300

    experience

--- xrextras/webpack.config.js
+++ xrextras/webpack.config.js
@@ -1,24 +1,33 @@
-const path = require('path')
+const path = require('path');
 
 module.exports = {
   entry: './src/index.js',
   output: {
     filename: 'xrextras.js',
-    path: path.resolve(__dirname, 'dist')
+    path: path.resolve(__dirname, 'dist'),
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
-      }, {
+        use: ['style-loader', 'css-loader'],
+      },
+      {
         test: /\.html$/,
         use: 'html-loader',
-      }
-    ]
+      },
+      {
+        test: /\.svg$/,
+        use: [
+          {
+            loader: 'svg-url-loader',
+            options: {
+              limit: 10000,
+            },
+          },
+        ],
+      },
+    ],
   },
-  mode: 'production'
-}
+  mode: 'production',
+};

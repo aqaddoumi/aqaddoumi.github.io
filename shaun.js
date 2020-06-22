09cfbbcc4eb349cdaad80e6adb8610bd2dbb34e8
aqaddoumi <qaddoumi.adel@gmail.com>
2020-06-23 00:43:27 +0300

    test

--- shaun.js
+++ shaun.js
@@ -1,5 +1,6 @@
 AFRAME.registerComponent('gift', {
   init: function () {
+    console.log('A');
     this.el.addEventListener('model-loaded', (e) => {
       console.log(this.el.components['gltf-model'].model.animations);
     });

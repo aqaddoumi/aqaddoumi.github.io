08ac1044063a7bb69fcea46abdba9de90d5aa0d8
aqaddoumi <qaddoumi.adel@gmail.com>
2020-07-06 12:48:56 +0300

    arjs

---
+++ notes.js
@@ -0,0 +1,15 @@
+//Load ARjs
+//Create User Interface
+//Create Loading
+//Register NFT Event
+//Register Model Event
+//Register Video Event
+//Take User Input
+//Tune Gift Animation
+//Tune Cat Animation
+//Tune Text Styling
+//Tune Text Animation
+//Tune Particles
+//Play Music
+//Tune Down Music
+//Clean Up Code

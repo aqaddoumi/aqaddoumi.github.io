a04e9ffef1ae2987d203607672f9da8e48988430
aqaddoumi <qaddoumi.adel@gmail.com>
2020-11-03 00:18:45 +0200

    fix to pattern

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var params = {
 	host: "0.0.0.0", // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
 	root: "./", // Set root directory that's being served. Defaults to cwd.
 	open: true, // When false, it won't load your browser by default.
-	file: "index.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
+	file: "index1.html", // When set, serve this file (server root relative) for every 404 (useful for single-page applications)
 	wait: 1000, // Waits for all changes, before reloading. Defaults to 0 sec.
 	mount: [['/components', './node_modules']], // Mount a directory to a route.
 	logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
